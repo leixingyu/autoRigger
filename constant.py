f233874c2dd3b3421828ae06dc8fd8794bb0260d
leixingyu <techartlei@gmail.com>
2021-12-02 21:08:59 -0500

    added direction to constant

--- constant.py
+++ constant.py
@@ -21,3 +21,14 @@ class RigType(IntEnum):
     QUADRUPED = 1
     CHAIN = 2
     CUSTOM = 3
+
+
+@unique
+class Direction(Enum):
+    Y_POSITIVE = [0, 1, 0]
+    Y_NEGATIVE = [0, -1, 0]
+    X_POSITIVE = [1, 0, 0]
+    X_NEGATIVE = [-1, 0, 0]
+    Z_POSITIVE = [0, 0, 1]
+    Z_NEGATIVE = [0, 0, -1]
+
